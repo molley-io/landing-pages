11f493cfa44cc527b75c8c1c5a8776909d9525fc
John Carmichael <johncarmichael1984@gmail.com>
2026-05-23 11:14:47 +0100

    fix: analytics build

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { ViteSSG } from 'vite-ssg'
-import VueMatomo from 'vue-matomo'
 import App from './App.vue'
 import { routes } from './routes'
 import './styles/variables.scss'
 import './styles/global.scss'
 
-export const createApp = ViteSSG(App, { routes }, ({ app, router, isClient }) => {
+export const createApp = ViteSSG(App, { routes }, async ({ app, router, isClient }) => {
   if (isClient && import.meta.env.VITE_MATOMO_ENABLED === 'true') {
+    const { default: VueMatomo } = await import('vue-matomo')
     app.use(VueMatomo, {
       host: import.meta.env.VITE_MATOMO_HOST,
       siteId: parseInt(import.meta.env.VITE_MATOMO_SITE_ID) || 1,
